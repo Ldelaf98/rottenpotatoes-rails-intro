54ffbdc04798279dd612d011bf0081ceda6419b1
Ubuntu <ubuntu@ip-172-31-34-196.us-east-2.compute.internal>
2020-03-10 03:08:05 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,9 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        ratings = Array.new
-        self.select("rating").uniq.each {|x| ratings.push(x.rating)}
-        ratings.sort.uniq
-        #Array['G','PG','PG-13','R', 'NC-17']    
+        # ratings = Array.new
+        # self.select("rating").uniq.each {|x| ratings.push(x.rating)}
+        # ratings.sort.uniq
+        
+        Array['G','PG','PG-13','R', 'NC-17']    
     end
 end
